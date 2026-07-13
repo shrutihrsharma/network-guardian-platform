18e85a78f24795d6fb1f584f4fb7f0153ee58f22
Shruti Sharma <shrutihrsharma@gmail.com>
2026-07-13 06:38:30 +0000

    adding cors config

---
+++ backend/src/main/java/com/networkguardian/backend/configuration/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.networkguardian.backend.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(frontendUrl)
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
