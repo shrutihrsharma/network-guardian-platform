28e55f1868737201599724dcf1ba80fb193a7308
Shruti Sharma <shrutihrsharma@gmail.com>
2026-07-23 10:24:48 +0000

    issue fixed for second navigation

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiBaseUrl: 'https://network-guardian-platform-production.up.railway.app/api',
   googleClientId: '688364170240-21p9plosk9ikmmpc51ofbj3126b8lkue.apps.googleusercontent.com',
-  frontendReactLandingUrl: '/'
+  frontendReactLandingUrl: 'https://predict-prevent-prioritize.up.railway.app/'
 };
