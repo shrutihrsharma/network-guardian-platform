e55d4cd3a1d2132b7c76d2a95c5b6a56677c9e6b
Shruti Sharma <shrutihrsharma@gmail.com>
2026-08-18 17:10:51 +0000

    new google client id added

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://network-guardian-platform.onrender.com/api',
-  googleClientId: '688364170240-21p9plosk9ikmmpc51ofbj3126b8lkue.apps.googleusercontent.com',
+  googleClientId: '373480742611-408iafvk9evtq2081n3m5mofiad1dsfq.apps.googleusercontent.com',
   frontendReactLandingUrl: 'https://predict-prevent-prioritize.up.railway.app/'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'https://network-guardian-platform.onrender.com/api',
-  googleClientId: '688364170240-21p9plosk9ikmmpc51ofbj3126b8lkue.apps.googleusercontent.com',
+  googleClientId: '373480742611-408iafvk9evtq2081n3m5mofiad1dsfq.apps.googleusercontent.com',
   frontendReactLandingUrl: 'http://localhost:5173/'
 };
