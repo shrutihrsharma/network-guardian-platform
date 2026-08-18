5aef12b5fe39a57142370831cc6c88c16fd94941
Shruti Sharma <shrutihrsharma@gmail.com>
2026-08-18 16:43:39 +0000

    updated backend api url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://network-guardian-platform-production.up.railway.app/api',
+  apiBaseUrl: 'https://network-guardian-platform.onrender.com/api',
   googleClientId: '688364170240-21p9plosk9ikmmpc51ofbj3126b8lkue.apps.googleusercontent.com',
   frontendReactLandingUrl: 'https://predict-prevent-prioritize.up.railway.app/'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://network-guardian-platform-production.up.railway.app/api',
+  apiBaseUrl: 'https://network-guardian-platform.onrender.com/api',
   googleClientId: '688364170240-21p9plosk9ikmmpc51ofbj3126b8lkue.apps.googleusercontent.com',
   frontendReactLandingUrl: 'http://localhost:5173/'
 };
